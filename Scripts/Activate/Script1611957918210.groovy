3a06d5b0cf3fb282e9953835938ef77cd32f058a
User <User@DESKTOP-N1UAA3R>
2021-01-30 20:09:55 +0100

    Added Test for Account activation

--- Scripts/Activate/Script1611957918210.groovy
+++ Scripts/Activate/Script1611957918210.groovy
@@ -15,4 +15,48 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
+import io.appium.java_client.android.nativekey.AndroidKey as androidKey
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory as MobileDriverFactory
+import io.appium.java_client.windows.PressesKeyCode as PressesKeyCode
+import io.appium.java_client.android.AndroidDriver as AndroidDriver
+
+Mobile.startApplication('C:\\Users\\User\\Downloads\\carbon_ng-v6.7.1_signed.apk', true)
+
+if (Mobile.verifyElementVisible(findTestObject('Object Repository/Activate/android.widget.TextView - Payments madeeasy'), 
+    0, FailureHandling.OPTIONAL) == true) {
+    Mobile.tap(findTestObject('Object Repository/Activate/android.widget.Button - Skip'), 0)
+}
+
+Mobile.delay(60, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Activate/android.widget.Button - Sign In'), 90)
+
+Mobile.setText(findTestObject('Object Repository/Activate/android.widget.EditText - Phone number'), '08990001100', 0)
+
+Mobile.setText(findTestObject('Object Repository/Activate/android.widget.EditText - Enter PIN'), '1234', 0)
+
+Mobile.tap(findTestObject('Object Repository/Activate/android.widget.Button - Sign in (1)'), 0)
+
+if (Mobile.verifyElementVisible(findTestObject('Object Repository/Activate/android.widget.Button - Apply for a loan'), 0, 
+    FailureHandling.OPTIONAL) == true) {
+    KeywordUtil.markPassed('The app has been activated and has logged in')
+} else {
+    AndroidDriver<?> driver = MobileDriverFactory.getDriver()
+
+    driver.pressKeyCode(8)
+
+    driver.pressKeyCode(9)
+
+    driver.pressKeyCode(10)
+
+    driver.pressKeyCode(11)
+
+    driver.pressKeyCode(12)
+
+    driver.pressKeyCode(13)
+
+    Mobile.delay(10)
+}
 
